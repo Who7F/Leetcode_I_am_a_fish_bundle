e19db0719d69aa346f8067f4daead2fa08d2586d
Who7F <william.hoggins@googlemail.com>
2024-11-19 11:48:26 +0000

    min heap

---
+++ 0215 Kth Largest Element in an Array/cplusone.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+
+        for (int i = 0; i < k; i++)
+            min_heap.push(nums[i]);
+        
+
+        for(int i = k; i < nums.size(); i++)
+            if(nums[i] > min_heap.top()){
+                min_heap.pop();
+                min_heap.push(nums[i]);
+            }
+            
+
+        return min_heap.top();
+    }
+};
