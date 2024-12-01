a5db632a4c8812e2366c4984fd2c056800511607
Who7F <william.hoggins@googlemail.com>
2024-12-01 23:11:42 +0000

    k freq

---
+++ 0347 Top K Frequent Elements/p3one.py
@@ -0,0 +1,12 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        heap = []
+
+        for key, val in counter.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (val, key))
+            else:
+                heapq.heappushpop(heap, (val, key))
+
+        return [i[1] for i in heap]
