f4e5228152e1ee465b848c0e202e2733a5686788
Who7F <william.hoggins@googlemail.com>
2026-05-06 23:31:08 +0100

    nothing really to say about this one

---
+++ 1848 Minimum Distance to the Target Element/py3one.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        res = float("inf")
+        for i, n in enumerate(nums):
+
+            if n == target:
+                res = min(res, abs(start - i))
+
+        return res
