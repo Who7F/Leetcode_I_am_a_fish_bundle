c20a8b408907d6dc4f262bc70f0af42a0ab76bff
Who7F <william.hoggins@googlemail.com>
2026-07-21 20:58:16 +0100

    greedy prefix-max/suffix-min

--- 3660 Jump Game IX/py3one.py
+++ 3660 Jump Game IX/py3one.py
@@ -1,17 +1,18 @@
 class Solution:
-    def maxValue(self, A: List[int]) -> List[int]:
-        n = len(A)
-        pst = A[:]
+    def maxValue(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        sfx = nums[:]
         
         for i in range(n - 2, -1, -1):
-            pst[i] = min(pst[i], pst[i + 1])
+            sfx[i] = min(sfx[i], sfx[i + 1])
 
         res = []
         m = 0
-        for i, x in enumerate(A):
+        for i, x in enumerate(nums):
             m = max(m, x)
-            if i == n - 1 or m <= pst[i + 1]:
+            if i == n - 1 or m <= sfx[i + 1]:
                 res.extend([m] * (i + 1 - len(res)))
                 m = 0
+
 
         return res
